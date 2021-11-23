e335978904f5401cb22e08fe2c14dda3f074b753
Denzel <denzelsamala@Denzel.local>
2021-11-23 09:02:46 -0800

    status intervals set in place

--- js/app.js
+++ js/app.js
@@ -8,5 +8,57 @@ function storeName () {
     const knightname = $("#namebox2").val();
     $('#namebox1').html("Knight " + knightname);
 
-};
+}
 
+let knight = {
+    name: '',
+    hunger: 0,
+    fatigue: 0,
+    skill: 100,
+    age: 1,
+}
+
+
+ let $hungerProgress = $('#hungerProgress');
+ let $fatigueProgress = $('#fatigueProgress');
+ let $swordProgress = $('#swordProgress');
+ let $age = $('#age');
+
+
+        setInterval(function() {
+            
+            if(knight.hunger >= 100) {
+                clearInterval(setInterval($hungerProgress.css('width') = 0));
+
+            } else {
+                knight.hunger++;
+                $hungerProgress.css('width', knight.hunger + '%');
+            }
+        }, 1000);
+
+        setInterval(function() {
+            
+            if(knight.fatigue >= 100) {
+                clearInterval(setInterval($fatigueProgress.css('width') = 0));
+
+            } else {
+                knight.fatigue++;
+                $fatigueProgress.css('width', knight.fatigue + '%');
+            }
+        }, 1500);
+
+        setInterval(function() {
+            
+            if(knight.skill <= 0) {
+                clearInterval(setInterval($fatigueProgress.css('width') = 0));
+
+            } else {
+                knight.skill--;
+                $swordProgress.css('width', knight.skill + '%');
+            }
+        }, 3000);
+
+        setInterval(function () {
+            knight.age++;
+            $age.html('Days Survived: ' + knight.age);
+        }, 2500)
